3862d0ebdddeca979bce7a9986a4fea6fed36e64
Nir <nirborger@mail.tau.ac.il>
2022-04-19 14:42:26 +0300

    updated dimensions and need to fix read_file

---
+++ kmeans.c
@@ -0,0 +1,124 @@
+/* Assignment 1 - Software Project
+In this assignment we were asked to implement k-means algorithm in python and in C.
+Provided here the C implementation.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int first_input_validation(int length_of_input, char *input[])
+{
+    if (length_of_input != 4 && length_of_input != 5){
+        printf("Invalid Input!");
+        return 1;
+    }
+    if (atoi(input[1]) == 0 || (length_of_input == 5 && atoi(input[2]) == 0)) {
+        printf("Invalid Input!");
+        return 1;
+    }
+    return 0;
+}
+
+void find_dimensions(char const *filename, int *dims){
+    FILE *f = NULL;
+    char c;
+    f = fopen(filename, "r");
+    if (f == NULL) {
+        fclose(f);
+        return;
+    }
+    dims[0] = 0;
+    dims[1] = 1;
+    while ((c = fgetc(f)) != EOF) 
+    {
+        if (c == '\n')
+        {
+            dims[0]++;
+        }
+        else if (c == ',')
+        {
+            if (dims[0] == 0)
+            {
+                dims[1]++;
+            }
+        }
+    }
+    fclose(f);
+}
+
+
+/*double** read_file(char const *filename) 
+{
+    FILE *f = NULL;
+    char c;
+    int columns, rows, i, j;
+    double** obs;
+    f = fopen(filename, "r");
+    if (f == NULL) {
+        return NULL;
+    }
+    /Consider adding a command to get to the next row.
+    rewind(f);
+    obs = calloc(rows, sizeof(double*));
+    for (i = 0; i < rows; i++)
+    {
+        obs[i] = calloc(columns, sizeof(double));
+        for (j = 0; j < columns; j++)
+        {
+            fscanf(f, "%lf%c", &obs[i][j], &c);
+            Consider adding a command to get to the next row.
+        }
+    }
+    fclose(f);
+    return obs;
+} */
+
+int second_input_validation(int K, int N, int max_iter)
+{
+    if(!(1<K && K<N) || max_iter <= 0) {
+        printf("%d, %d, %d", K, N, max_iter);
+        printf("Invalid Input!");
+        printf("second_input_validation");
+        return 1;
+    }
+    return 0;
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+    /*double** obs;*/
+    int K, max_iter, i;
+    char* input_file_path;
+    int dims[2];
+    /*char* output_file_path;*/
+    if(first_input_validation(argc, argv) == 1){
+        return 1;
+    }
+    K = atoi(argv[1]);
+    max_iter = 200;
+    i = 1;
+    if (argc == 5) {
+        i++;
+        max_iter = atoi(argv[i]);
+    }
+    input_file_path = argv[i+1];
+    /*output_file_path = argv[i+2];*/
+    /*obs = read_file(input_file_path);    
+    if (obs == NULL) Condition may be irrelevant, consider deleting.
+    {
+        return 1;
+    }*/
+    find_dimensions(input_file_path, dims);
+    printf("%d, %d, \n", dims[0], dims[1]);
+
+    if (second_input_validation(K, dims[0], max_iter) == 1)
+    {
+        return 1;
+    }
+    printf("%d, %d", dims[0], dims[1]);
+    printf("Reached the end");
+    return 0;
+}
